4bc8d6ebbc3cfab33c7072c11ed43967c9817763
Apes on Parade <throwra_scentsitive@reddit.com>
2021-07-26 23:47:17 -0400

    (testing) Config file approach

--- js/reddit.mjs
+++ js/reddit.mjs
@@ -1,3 +1,11 @@
-export default function reddit(){
-	alert('ok')
+export default async function reddit(){
+	const configResponse = await fetch('./config.json')
+	const config = await configResponse.json()
+	console.log(config)
+	if(config.verifyOwnDomain && document.location.origin == `https://${config.verifyOwnDomain}`){
+		alert('ok')
+		}
+	else {
+		alert(`This code will only run on ${config.verifyOwnDomain}. If you have forked this repository, please update your config.json`)
+		}
 	}
